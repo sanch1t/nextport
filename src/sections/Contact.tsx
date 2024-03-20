f256a0c5d286613c74fb16e8d53e572dad8d0dc3
Sanchit <64640799+sanch1t@users.noreply.github.com>
2024-03-20 19:42:01 +0530

    Content Update

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -18,9 +18,9 @@ function Contact() {
       <h2 className="contact-title">What&apos;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        Although I’m not currently looking for any new opportunities, my inbox
+        I am always on the lookout for new oppurtunities, and my inbox
         is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+        get back to you ASAP!
       </p>
       <div className="contact-cta">
         <Button link="mailto:sanchitsingh1503@gmail.com" text="Say Hello" />
